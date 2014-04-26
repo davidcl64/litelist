c3aee6cd72ddc9292a78c54f647ccb0f6df5d1f4
David Clarke <davidcl64@gmail.com>
2014-04-25 20:35:11 -0700

    constructor tests

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,6 @@
-var bundle = require('../src/bundled');
+// Here to generate browserified code bundle
+require('../src/bundled');
+
 var chai   = require('chai');
 var sinon  = require('sinon');
 var expect = chai.expect;
@@ -7,10 +9,66 @@ var pkg    = require('../package.json');
 chai.expect();
 chai.should();
 
-describe(["LiteList"], function() {
-    describe("bundled", function() {
-        var LiteList = require('LiteList');
+var LiteList = require('LiteList');
+var mock     = require('./lib/mock');
 
+describe(["LiteList"], function() {
+    var dataSource = {
+        bind: function(/*id, el*/) {},
+
+        sync: function(id, el/*, itemIdx, item*/) {
+            el.innerText = "innerText[" + id + "]";
+        },
+
+        unbind: function(/*id, el*/) {}
+    };
+
+    var itemTemplate    = document.createDocumentFragment();
+
+    var fullOpts = {
+        itemWidth:      100,
+        itemHeight:     100,
+        margin:         { x: 0, y: 0 },
+        scrollView:     ".scrollView",
+        itemsContainer: ".itemsContainer",
+        dataSource:     dataSource,
+        itemTemplate:   itemTemplate
+    };
+
+    var requiredOpts = {
+        itemHeight:     100,
+        scrollView:     ".scrollView"
+    };
+
+    var viewWidth = 1000, viewHeight = 1000;
+
+    var scrollView, itemsContainer;
+    var sandbox, qsStub, gcsStub;
+
+    beforeEach(function() {
+        scrollView      = mock.getFakeView(viewWidth, viewHeight);
+        itemsContainer  = scrollView.children[0];
+
+        itemTemplate.appendChild(document.createElement("div"));
+
+        // Create a new sandbox
+        sandbox = sinon.sandbox.create();
+
+        qsStub  = sandbox.stub(document, 'querySelector');
+        gcsStub = sandbox.stub(window,   'getComputedStyle');
+
+        qsStub.withArgs('.scrollView'    ).returns(scrollView);
+        qsStub.withArgs('.itemsContainer').returns(itemsContainer);
+
+        gcsStub.withArgs(scrollView).returns({ height: scrollView.clientHeight });
+    });
+
+    // Restore stuff to the way we were...
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    describe("bundled", function() {
         it("LiteList should exist", function() {
             expect(LiteList).to.exist;
         });
@@ -38,61 +96,54 @@ describe(["LiteList"], function() {
     });
 
     describe("constructor", function() {
-        var LiteList   = require('LiteList');
-        var mock       = require('./lib/mock');
-        var dataSource = {
-            bind: function(/*id, el*/) {},
-
-            sync: function(id, el, itemIdx, item) {
-                el.innerText = "innerText[" + id + "]";
-            },
-
-            unbind: function(/*id, el*/) {}
-
-
-        };
-
-        var itemTemplate    = document.createDocumentFragment();
-
-        var fullOpts = {
-            itemWidth:      100,
-            itemHeight:     100,
-            margin:         { x: 0, y: 0 },
-            scrollView:     ".scrollView",
-            itemsContainer: ".itemsContainer",
-            dataSource:     dataSource,
-            itemTemplate:   itemTemplate
-        };
-
-        var scrollView, itemsContainer;
-        var sandbox, qsStub, gcsStub;
-
-        beforeEach(function() {
-            scrollView      = mock.getFakeView(1000, 1000);
-            itemsContainer  = scrollView.children[0];
-
-            itemTemplate.appendChild(document.createElement("div"));
-
-            // Create a new sandbox
-            sandbox = sinon.sandbox.create();
-
-            qsStub  = sandbox.stub(document, 'querySelector');
-            gcsStub = sandbox.stub(window,   'getComputedStyle');
-
-            qsStub.withArgs('.scrollView'    ).returns(scrollView);
-            qsStub.withArgs('.itemsContainer').returns(itemsContainer);
-
-            gcsStub.withArgs(scrollView).returns({ height: scrollView.clientHeight });
-        });
-
-        // Restore stuff to the way we were...
-        afterEach(function () {
-            sandbox.restore();
-        });
-
-        it("Should create a LiteListObject with all opt specified", function() {
+        it("Should create a LiteList object with all options specified", function() {
             var liteList = new LiteList(fullOpts);
-            console.log(liteList);
+
+            expect(liteList.itemsInView).to.be.instanceof(Array);
+            expect(liteList.itemsInView.length).to.equal(0);
+            expect(liteList.items).to.be.instanceof(Array);
+            expect(liteList.items.length).to.equal(0);
+            expect(liteList.itemWidth).to.equal(fullOpts.itemWidth);
+            expect(liteList.itemHeight).to.equal(fullOpts.itemHeight);
+            expect(liteList.margin).to.deep.equal(fullOpts.margin);
+            expect(liteList.view).to.deep.equal(scrollView);
+            expect(liteList.itemsContainer).to.deep.equal(itemsContainer);
+            expect(liteList.dataSource).to.deep.equal(dataSource);
+            expect(liteList.itemTemplate).to.deep.equal(itemTemplate);
+            expect(liteList.scrollTop).to.equal(0);
+
+            // View Metrics
+            expect(liteList.clientHeight).to.equal(viewHeight);
+            expect(liteList.clientWidth ).to.equal(viewWidth);
+            expect(liteList.rowsPerPage ).to.equal(viewHeight/fullOpts.itemHeight);
+            expect(liteList.itemsPerRow ).to.equal(viewWidth /fullOpts.itemWidth );
+            expect(liteList.itemsPerPage).to.equal(liteList.rowsPerPage * liteList.itemsPerRow);
+            expect(liteList.maxBuffer   ).to.equal(liteList.itemsPerPage * 3);
+        });
+
+        it("Should use expected defaults when only required opts are specified", function() {
+            var liteList = new LiteList(requiredOpts);
+
+            expect(liteList.itemsInView).to.be.instanceof(Array);
+            expect(liteList.itemsInView.length).to.equal(0);
+            expect(liteList.items).to.be.instanceof(Array);
+            expect(liteList.items.length).to.equal(0);
+            expect(liteList.itemWidth).to.equal(0);
+            expect(liteList.itemHeight).to.equal(requiredOpts.itemHeight);
+            expect(liteList.margin).to.deep.equal({x: 0, y: 0});
+            expect(liteList.view).to.deep.equal(scrollView);
+            expect(liteList.itemsContainer).to.deep.equal(itemsContainer);
+            expect(liteList.dataSource).to.equal(false);
+            expect(liteList.itemTemplate).to.equal(false);
+            expect(liteList.scrollTop).to.equal(0);
+
+            // View Metrics
+            expect(liteList.clientHeight).to.equal(viewHeight);
+            expect(liteList.clientWidth ).to.equal(viewWidth);
+            expect(liteList.rowsPerPage ).to.equal(viewHeight/fullOpts.itemHeight);
+            expect(liteList.itemsPerRow ).to.equal(1);
+            expect(liteList.itemsPerPage).to.equal(liteList.rowsPerPage * liteList.itemsPerRow);
+            expect(liteList.maxBuffer   ).to.equal(liteList.itemsPerPage * 3);
         });
     });
 });
